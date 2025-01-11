eabe28a681db4976fe2b9f897f2307ae3d2d43c7
Wectornanime <wectornanime@gmail.com>
2025-01-11 16:37:17 -0300

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import router from "./router";
+import swaggerUi from "swagger-ui-express";
+import swaggerJson from './swagger.json';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerJson))
 app.use(router);
 
 export default app;
